a77e8429c5eff2d0f49775e2fe27ee729b6a8de0
Nhiaz <fitti0587@gmail.com>
2024-05-23 06:23:44 +1200

    project

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const placesCard = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+function addCard(title, imageLink, deleteCard) {
+    const cardElement = cardTemplate.cloneNode(true);
 
-// @todo: Функция создания карточки
+    cardElement.querySelector('.card__title').textContent = title;
+    cardElement.querySelector('.card__image').src = imageLink;
+    cardElement.querySelector('.card__image').alt = title;
 
-// @todo: Функция удаления карточки
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
 
-// @todo: Вывести карточки на страницу
+    return cardElement;
+}
+
+function deleteCard(evt) {
+    evt.target.closest('.places__item').remove();
+}
+
+for (i = 0; i < initialCards.length; i++) {
+    const cardElements = addCard(initialCards[i].name, initialCards[i].link, deleteCard);
+    placesCard.append(cardElements);
+}
